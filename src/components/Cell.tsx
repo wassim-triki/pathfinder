4b608ae48b1f6969f4e6c88d48400f5da3f3faad
Wassim Triki <wsmtriki@gmail.com>
2023-01-16 02:34:34 +0100

    add basic animations

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { ICell } from '../interfaces/ICell';
 
 interface CellProps {
@@ -8,14 +8,27 @@ interface CellProps {
 const Cell: FunctionComponent<CellProps> = ({ cell }: CellProps) => {
   const { isStart, isTarget, isWall, isNeighbor, visited } = cell;
 
+  const [isMouseDown, setIsMouseDown] = useState(false);
+
+  const handleMouseDown = () => {
+    setIsMouseDown(true);
+    if (!cell.isTarget && !cell.isStart) cell.isWall = !cell.isWall;
+  };
+
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+
   return (
     <div
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
       className={`border-[1px] bg-{} border-light-100 w-8 h-8 cursor-pointer ${
         !isStart && !isTarget && !isWall && 'hover:bg-light-100'
-      }  active:transform active:scale-105 activex:bg-white transition-transform active:border-white 
-      ${visited && !isStart && !isTarget && 'bg-blue-400'}
+      }  active:border-white 
+      ${visited && !isStart && !isTarget && 'bg-blue-400 animate-scale '}
       ${isNeighbor && !visited && !isTarget && 'bg-blue-200'}
-      ${isWall && 'bg-white'}
+      ${isWall && 'bg-white animate-scale '}
       ${isTarget && 'bg-red-400'}
       ${isStart && 'bg-green-400'}  
       `}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,16 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      keyframes: {
+        scale: {
+          '0%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.2)' },
+          '100%': { transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        scale: 'scale .4s cubic-bezier(.39,.575,.565,1.000) both',
+      },
       colors: {
         dark: {
           100: '#13111C',
