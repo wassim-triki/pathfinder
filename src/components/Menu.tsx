f8c2ebe17d144b49689dc3d8f56ceccbc74bd2aa
Wassim Triki <wsmtriki@gmail.com>
2023-01-18 03:12:20 +0100

    add icons

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useAstar } from '../hooks/useAstar';
 import { useGridContext } from '../context/gridContext';
-
+import { RiRestartLine } from 'react-icons/ri';
+import { AiOutlinePlayCircle } from 'react-icons/ai';
+import { TbWall } from 'react-icons/tb';
+import { RiRouteLine } from 'react-icons/ri';
 const Menu = () => {
   const { grid, setGrid, startSearch, resetGrid, clearPath, clearWalls } = useGridContext();
 
@@ -11,19 +14,23 @@ const Menu = () => {
     <div className='flex justify-center  items-center flex-col gap-2 px-0'>
       <div className='flex gap-2 w-full justify-between'>
         <button className='btn flex-1' onClick={() => startSearch()}>
-          Start
+          <AiOutlinePlayCircle className='icon' />
+          <span> Start</span>
         </button>
         <button className='btn flex-1' onClick={() => resetGrid()}>
-          Reset
+          <RiRestartLine className='icon' />
+          <span>Reset</span>
         </button>
       </div>
 
       <div className='flex gap-2 w-full justify-between'>
         <button className='btn flex-1' onClick={() => clearPath()}>
-          Clear Path
+          <RiRouteLine className='icon' />
+          <span>Clear Path</span>
         </button>
         <button className='btn flex-1' onClick={() => clearWalls()}>
-          Clear Walls
+          <TbWall className='icon' />
+          <span> Clear Walls</span>
         </button>
       </div>
     </div>
